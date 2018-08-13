8db2d2a00f799d14c9b74393c2fdf45d205a8d24
rutvik2007 <40671458+rutvik2007@users.noreply.github.com>
2018-08-13 19:49:17 +0530

    Update Mult.asm

--- Project4/Mult.asm
+++ Project4/Mult.asm
@@ -2,31 +2,24 @@
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// Put your code here.
 
-        @sum    
-        M=0     // Initialize sum to 0
-        @R1
-        D=M
-        @count  
-        M=D     // Initialize count to the value at RAM[1]
-(LOOP)  
-        @count
-        D=M
-        @END
-        D;JEQ   // Check if count is 0, if so, goto END
-        @R0
-        D=M
-        @sum
-        M=M+D   // Add RAM[0] to sum
-        @count
-        M=M-1   // Decrement count by 1
-        @LOOP
-        0;JMP   // Loop again
-(END)   
-        @sum
-        D=M
-        @R2
-        M=D     // Write sum to RAM[2]
+@R2
+	M=0             //Initialize R2 to 0
+(LOOP)
+	@R1
+		M=M-1   //Decrement R1
+		D=M     //D=R1
+	@EXIT
+		D;JLT   //Branch to EXIT if D<0
+	@R0
+		D=M     //
+	@R2
+		M=D+M   //R2=R1+R2 (for repeated addition)
+	@LOOP
+		0;JMP   //Unconditional jump to LOOP
+(EXIT)
+	@EXIT
+		0;JMP   //Infinite loop to terminate program
